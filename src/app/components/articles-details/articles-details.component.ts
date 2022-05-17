19f2e4c965ab3974d9acb187a132e1bee574b935
Dmytrii9Cherniak <dcheyrnak10@gmail.com>
2022-05-17 12:24:42 +0300

    added articles and routing to dynamic pages

---
+++ src/app/components/articles-details/articles-details.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+import {ArticleModel} from "../../models/articleModel";
+import {ArticleService} from "../../services/article.service";
+import {ActivatedRoute} from "@angular/router";
+
+@Component({
+  selector: 'app-articles-details',
+  templateUrl: './articles-details.component.html',
+  styleUrls: ['./articles-details.component.scss']
+})
+export class ArticlesDetailsComponent implements OnInit {
+
+  fullArticle: ArticleModel;
+
+  constructor(private articleService: ArticleService, private activatedRoute: ActivatedRoute) { }
+
+  ngOnInit(): void {
+    this.activatedRoute.params.subscribe(params => {
+      this.articleService.getArticleById(params['id']).subscribe(value => this.fullArticle = value);
+    })
+  }
+
+}
